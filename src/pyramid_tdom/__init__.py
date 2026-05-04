44f1511a166d4ccebf2e4c6353efb5be66a2113a
Ian Wilson <ian@gardentheory.com>
2026-05-03 21:40:54 -0700

    Allow multiple ctx to be set based on ret value of view callable.

--- src/pyramid_tdom/__init__.py
+++ src/pyramid_tdom/__init__.py
@@ -1,6 +1,7 @@
-from contextvars import ContextVar
+from contextvars import ContextVar, Token
 from dataclasses import dataclass, field
 from typing import Protocol
+from string.templatelib import Template
 
 from tdom.processor import ProcessContext, TemplateProcessor, IComponentProcessor, ComponentProcessor
 
@@ -24,10 +25,47 @@ class TdomRenderer:
 
     def __call__(self, info):
         def render_func(value, system):
-            with PyramidTdomCtx.set(system):
-                return self.processor_api.process(
-                    value, assume_ctx=_default_processor_ctx)
+            if isinstance(value, Template):
+                with PyramidTdomCtx.set(system):
+                    return self.processor_api.process(
+                        value, assume_ctx=_default_processor_ctx)
+            else:
+                # View function can delegate setting context variable values
+                # during the render.  Just fail here if the wrong thing was returned
+                # maybe we can do something smarter later that is still "fast-like".
+                value_t, cvals = value
+                with PyramidTdomCtx.set(system), ContextVarSetter(context_values=cvals):
+                    return self.processor_api.process(
+                        value_t, assume_ctx=_default_processor_ctx)
         return render_func
+
+
+class ContextVarSetter:
+    """
+    Context manager for working with many context vars (instead of only 1).
+
+    This is meant to be created, used immediately and then discarded.
+
+    This allows for dynamically specifying a tuple of var / value pairs that
+    another part of the program can use to wrap some called code without knowing
+    anything about either.
+    """
+
+    context_values: tuple[tuple[ContextVar, object], ...]  # Cvar / value pair.
+    tokens: tuple[Token, ...]
+
+    def __init__(self, context_values=()):
+        self.context_values = context_values
+        self.tokens = ()
+
+    def __enter__(self):
+        """Set every given context var to its paired value."""
+        self.tokens = tuple(var.set(val) for var, val in self.context_values)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Reset every given context var."""
+        for idx, var_value in enumerate(self.context_values):
+            var_value[0].reset(self.tokens[idx])
 
 
 @dataclass
